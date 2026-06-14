03afa36baea497aa40b4d29944a869de3704f280
SahilRajput2609 <ratinkumar123123@gmail.com>
2026-06-14 13:22:57 +0530

    Implement audio player controls and now playing info

--- music-app/ios/MusicApp/Services/AudioPlayerService.swift
+++ music-app/ios/MusicApp/Services/AudioPlayerService.swift
@@ -32,3 +32,67 @@ class AudioPlayerService: NSObject, ObservableObject {
         
         commandCenter.playCommand.addTarget { [weak self] _ in
             self?.play()
+            return .success
+        }
+        
+        commandCenter.pauseCommand.addTarget { [weak self] _ in
+            self?.pause()
+            return .success
+        }
+        
+        commandCenter.nextTrackCommand.addTarget { _ in
+            NotificationCenter.default.post(name: NSNotification.Name("nextTrack"), object: nil)
+            return .success
+        }
+        
+        commandCenter.previousTrackCommand.addTarget { _ in
+            NotificationCenter.default.post(name: NSNotification.Name("previousTrack"), object: nil)
+            return .success
+        }
+    }
+    
+    func loadTrack(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        
+        duration = playerItem.asset.duration.seconds
+        
+        timeObserver = player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: .main) { [weak self] time in
+            self?.currentTime = time.seconds
+            self?.updateNowPlayingInfo()
+        }
+        
+        setupNowPlayingInfo()
+    }
+    
+    func play() {
+        player?.play()
+        isPlaying = true
+    }
+    
+    func pause() {
+        player?.pause()
+        isPlaying = false
+    }
+    
+    func seek(to time: TimeInterval) {
+        player?.seek(to: CMTime(seconds: time, preferredTimescale: 1))
+    }
+    
+    private func setupNowPlayingInfo() {
+        var nowPlayingInfo = [String: Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "Current Track"
+        nowPlayingInfo[MPMediaItemPropertyArtist] = "Artist Name"
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = currentTime
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = duration
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1.0
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+    
+    private func updateNowPlayingInfo() {
+        var nowPlayingInfo = MPNowPlayingInfoCenter.default().nowPlayingInfo ?? [:]
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = currentTime
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+}
